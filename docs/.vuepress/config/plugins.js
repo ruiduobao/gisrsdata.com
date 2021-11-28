0bc017229e19cce38998e92d111820dea89c4f2f
‘ruiduobao’ <kitmyfaceplease2@gmail.com>
2021-11-29 01:52:42 +0800

    百度推送修改

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -33,7 +33,7 @@ module.exports = [
         // },
         {
           title: '通过百度搜索本站的',
-          frontUrl: 'https://www.baidu.com/s?wd=site%3Axugaoyi.com%20',
+          frontUrl: 'https://www.baidu.com/s?wd=site%www.ruiduobao.com%20',
         },
       ],
     },
