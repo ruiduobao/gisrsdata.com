f9232d9f186b8158e4da066e5db1d25c7f9c84fc
‘ruiduobao’ <kitmyfaceplease2@gmail.com>
2021-11-28 23:32:47 +0800

    添加百度统计

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -11,6 +11,21 @@ module.exports = [
   ],
   ['meta', { name: 'baidu-site-verification', content: 'xxx' }], // 百度统计的站长验证
   ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+  
+      // 添加百度统计
+[
+        "script",
+        {},
+        `
+        var _hmt = _hmt || [];
+        (function() {
+          var hm = document.createElement("script");
+          hm.src = "https://hm.baidu.com/hm.js?6bf5928f8f71a10667000834a503ff61";
+          var s = document.getElementsByTagName("script")[0];
+          s.parentNode.insertBefore(hm, s);
+        })();
+          `
+]
   //[
   //  'script',
   //  {
