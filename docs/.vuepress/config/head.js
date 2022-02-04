bf10d7118ad79429a9e5a84168d10e7b5a0590fe
锐多宝的地理空间 <a2281138561@foxmail.com>
2022-02-05 01:08:13 +0800

    1

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -6,7 +6,7 @@ module.exports = [
     'meta',
     {
       name: 'keywords',
-      content: '地理信息系统,地信,遥感，GIS,RS,webgis,ENVI,postgis,',
+      content: '地理信息系统,地信,遥感,GIS,RS,地信遥感数据汇',
     },
   ],
   // [
@@ -36,7 +36,34 @@ module.exports = [
         s.parentNode.insertBefore(hm, s);
       })();
           `
+],
+//头条js提交收录
+[
+  "script",
+  {},
+  `
+  var el = document.createElement("script");
+  el.src = "https://lf1-cdn-tos.bytegoofy.com/goofy/ttzz/push.js?57b4417e5af27bb556862ddd1035af3b86d25aa934a0544184d9d963cc7f779465e0a2ada1d5e86b11e7de7c1a83287d04743a02fd1ee8dd8558a8cad50e91cb354f8c6f3f78e5fd97613c481f678e6d";
+  el.id = "ttzz";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(el, s);
+})();
+    `
 ]
+//360js提交收录
+[
+  "script",
+  {},
+  `
+  var src = "https://s.ssl.qhres2.com/ssl/ab77b6ea7f3fbf79.js";
+  document.write('<script src="' + src + '" id="sozz"><\/script>');
+  })();
+    `
+]
+
+
+
+
 
   //[
   //  'script',
