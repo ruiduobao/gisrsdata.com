fae621cb60b9a8bdf0da5961be0ec1fc050f9841
锐多宝的地理空间 <a2281138561@foxmail.com>
2022-04-29 10:50:52 +0800

    1

--- baiduPush.sh
+++ baiduPush.sh
@@ -5,5 +5,4 @@ set -e
 
 # 百度链接推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=https://www.gisrsdata.com&token=UF5wQ6TW3TrtoBt7"
-
 rm -rf urls.txt # 删除文件
