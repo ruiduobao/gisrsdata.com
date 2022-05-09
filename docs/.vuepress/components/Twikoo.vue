3b0ee52d04d1f097be15c4f8b2e3a7b7a76ba055
锐多宝的地理空间 <a2281138561@foxmail.com>
2022-05-09 11:27:31 +0800

    1

--- docs/.vuepress/components/Twikoo.vue
+++ docs/.vuepress/components/Twikoo.vue
@@ -63,7 +63,7 @@ export default {
     twikooInit() {
       twikoo
         .init({
-          envId: "https://wrikoo-1k9dtubnt-ruiduobao.vercel.app/",
+          envId: "https://wrikoo-5sn9hj8t5-ruiduobao.vercel.app/",
           el: "#tcomment",
           // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，如果您的环境地域不是上海，需传此参数
           // path: 'window.location.pathname', // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
